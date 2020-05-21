34db015a59bbb0c83811706dd21861d5c1721379
FATFATANT <291629631@qq.com>
2020-05-21 14:55:49 +0800

    initialize

---
+++ gulimall-myware/src/main/java/com/atguigu/gulimall/myware/GulimallMywareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.myware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMywareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallMywareApplication.class, args);
+    }
+
+}
---
+++ gulimall-myware/src/test/java/com/atguigu/gulimall/myware/GulimallMywareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.myware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMywareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
